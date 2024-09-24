5b7565f682c20a05bdac6cd1a502170c7af9061d
garvamel <garvamel@gmail.com>
2024-09-23 23:12:22 -0300

    Add extract md links and images from text

---
+++ src/test_inline markdown.py
@@ -0,0 +1,162 @@
+import unittest
+
+from textnode import (TextNode,
+                      split_nodes_delimiter,
+                      TextType,
+                      )
+
+from inline_markdown import extract_markdown_links, extract_markdown_images
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+
+    def test_not_text_type(self):
+
+        tnode = [TextNode("this is a **text**", TextType.bold),
+                 TextNode("this is a _text_", TextType.italic)]
+        new_nodes = split_nodes_delimiter(tnode, '', TextType.text)
+        # No delimiter and TextType.text shouldn't fail since tnode is not TextType.text
+        # It only tries to split TextType.text, other tyoes are not transformed.
+
+        self.assertEqual(new_nodes[0].text_type, TextType.bold)
+        self.assertEqual(new_nodes[1].text_type, TextType.italic)
+
+    def test_no_matching_delimiter(self):
+        tnode = [TextNode("this is a **text", TextType.text)]
+
+        with self.assertRaises(ValueError) as cm:
+            new_nodes = split_nodes_delimiter(tnode, '**', TextType.bold)
+        self.assertEqual(cm.exception.args[0], f"Invalid markdown syntax: ** missing closing character")
+
+    def test_expected_result(self):
+
+        tnode = [TextNode("this is a **text**", TextType.bold),
+                 TextNode("*this* is a text", TextType.italic),
+                 TextNode("this *is* a text", TextType.text),
+                TextNode("this is a `code`", TextType.code),
+
+                 ]
+        new_nodes = split_nodes_delimiter(tnode, '*', TextType.italic)
+        # No delimiter and TextType.text shouldn't fail since tnode is not TextType.text
+        # It only tries to split TextType.text, other tyoes are not transformed.
+
+        self.assertEqual(new_nodes[0].text_type, TextType.bold)
+        self.assertEqual(new_nodes[0].text, "this is a **text**")
+
+        self.assertEqual(new_nodes[1].text_type, TextType.italic)
+        self.assertEqual(new_nodes[1].text, "*this* is a text")
+
+        self.assertEqual(new_nodes[2].text_type, TextType.text)
+        self.assertEqual(new_nodes[2].text, "this ")
+        
+        self.assertEqual(new_nodes[3].text_type, TextType.italic)
+        self.assertEqual(new_nodes[3].text, "is")
+        
+        self.assertEqual(new_nodes[4].text_type, TextType.text)
+        self.assertEqual(new_nodes[4].text, " a text")
+        
+        self.assertEqual(new_nodes[5].text_type, TextType.code)
+        self.assertEqual(new_nodes[5].text, "this is a `code`")
+
+    def test_bold(self):
+
+        tnode = [TextNode("this **is** a **text**", TextType.text)]
+        new_nodes = split_nodes_delimiter(tnode, '**', TextType.bold)
+        # No delimiter and TextType.text shouldn't fail since tnode is not TextType.text
+        # It only tries to split TextType.text, other tyoes are not transformed.
+
+        self.assertEqual(new_nodes[0].text_type, TextType.text)
+        self.assertEqual(new_nodes[0].text, "this ")
+        
+        self.assertEqual(new_nodes[1].text_type, TextType.bold)
+        self.assertEqual(new_nodes[1].text, "is")
+        
+        self.assertEqual(new_nodes[2].text_type, TextType.text)
+        self.assertEqual(new_nodes[2].text, " a ")
+
+        self.assertEqual(new_nodes[3].text_type, TextType.bold)
+        self.assertEqual(new_nodes[3].text, "text")
+
+        # self.assertEqual(new_nodes[0].text_type, TextType.text)
+        # self.assertEqual(new_nodes[0].text, " a ")
+        # self.assertEqual(new_nodes[1].text_type, TextType.bold)
+        # self.assertEqual(new_nodes[1].text, "text")
+
+    def test_italic(self):
+
+        tnode = [TextNode("this *is* a text", TextType.text)]
+        new_nodes = split_nodes_delimiter(tnode, '*', TextType.italic)
+        # No delimiter and TextType.text shouldn't fail since tnode is not TextType.text
+        # It only tries to split TextType.text, other tyoes are not transformed.
+
+        self.assertEqual(new_nodes[0].text_type, TextType.text)
+        self.assertEqual(new_nodes[0].text, "this ")
+        
+        self.assertEqual(new_nodes[1].text_type, TextType.italic)
+        self.assertEqual(new_nodes[1].text, "is")
+        
+        self.assertEqual(new_nodes[2].text_type, TextType.text)
+        self.assertEqual(new_nodes[2].text, " a text")
+
+    def text_code(self):
+
+        tnode = [TextNode("this `is` a text", TextType.text)]
+        new_nodes = split_nodes_delimiter(tnode, '`', TextType.code)
+        # No delimiter and TextType.text shouldn't fail since tnode is not TextType.text
+        # It only tries to split TextType.text, other tyoes are not transformed.
+
+        self.assertEqual(new_nodes[0].text_type, TextType.text)
+        self.assertEqual(new_nodes[0].text, "this ")
+        
+        self.assertEqual(new_nodes[1].text_type, TextType.code)
+        self.assertEqual(new_nodes[1].text, "is")
+        
+        self.assertEqual(new_nodes[2].text_type, TextType.text)
+        self.assertEqual(new_nodes[2].text, " a text")
+
+    def test_multiple_calls(self):
+
+        tnode = [TextNode("this `is` a **text**", TextType.text)]
+        new_nodes = split_nodes_delimiter(tnode, '`', TextType.code)
+
+        self.assertListEqual(new_nodes, [ TextNode("this ", TextType.text),
+                                         TextNode("is", TextType.code),
+                                         TextNode(" a **text**", TextType.text)
+                                         ])
+
+
+        new_nodes2 = split_nodes_delimiter(new_nodes, '**', TextType.bold)
+
+        self.assertListEqual(new_nodes2, [TextNode("this ", TextType.text),
+                                         TextNode("is", TextType.code),
+                                         TextNode(" a ", TextType.text),
+                                         TextNode("text", TextType.bold)
+                                         ])
+
+class TestExtract(unittest.TestCase):
+    
+    def test_images_multiple(self):
+
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg). This is text with a link (treated as an image) ![to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)."
+        
+        self.assertEqual(extract_markdown_images(text),[("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+
+    def test_link_multiple(self):
+
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev). This is text with an image (treated as a link) [rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+
+        self.assertEqual(extract_markdown_links(text),[("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev"),("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+
+    def test_link_error(self):
+
+        #There's a space in between the [to boot dev] and (link url). Wrong syntaxt. Should only match the second link.
+
+        text = "This is text with a link [to boot dev] (https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)."
+
+        self.assertEqual(extract_markdown_links(text),[("to youtube","https://www.youtube.com/@bootdotdev")])
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,8 +2,6 @@ import unittest
 
 from textnode import (TextNode,
                       text_node_to_html_node,
-                      split_nodes_delimiter,
-                      TextType,
                       )
 
 
@@ -94,130 +92,6 @@ class TestTextNodeToHtml(unittest.TestCase):
             leaf_node = text_node_to_html_node(text_node)
 
 
-class TestSplitNodesDelimiter(unittest.TestCase):
-
-    def test_not_text_type(self):
-
-        tnode = [TextNode("this is a **text**", TextType.bold),
-                 TextNode("this is a _text_", TextType.italic)]
-        new_nodes = split_nodes_delimiter(tnode, '', TextType.text)
-        # No delimiter and TextType.text shouldn't fail since tnode is not TextType.text
-        # It only tries to split TextType.text, other tyoes are not transformed.
-
-        self.assertEqual(new_nodes[0].text_type, TextType.bold)
-        self.assertEqual(new_nodes[1].text_type, TextType.italic)
-
-    def test_no_matching_delimiter(self):
-        tnode = [TextNode("this is a **text", TextType.text)]
-
-        with self.assertRaises(ValueError) as cm:
-            new_nodes = split_nodes_delimiter(tnode, '**', TextType.bold)
-        self.assertEqual(cm.exception.args[0], f"Invalid markdown syntax: ** missing closing character")
-
-    def test_expected_result(self):
-
-        tnode = [TextNode("this is a **text**", TextType.bold),
-                 TextNode("*this* is a text", TextType.italic),
-                 TextNode("this *is* a text", TextType.text),
-                TextNode("this is a `code`", TextType.code),
-
-                 ]
-        new_nodes = split_nodes_delimiter(tnode, '*', TextType.italic)
-        # No delimiter and TextType.text shouldn't fail since tnode is not TextType.text
-        # It only tries to split TextType.text, other tyoes are not transformed.
-
-        self.assertEqual(new_nodes[0].text_type, TextType.bold)
-        self.assertEqual(new_nodes[0].text, "this is a **text**")
-
-        self.assertEqual(new_nodes[1].text_type, TextType.italic)
-        self.assertEqual(new_nodes[1].text, "*this* is a text")
-
-        self.assertEqual(new_nodes[2].text_type, TextType.text)
-        self.assertEqual(new_nodes[2].text, "this ")
-        
-        self.assertEqual(new_nodes[3].text_type, TextType.italic)
-        self.assertEqual(new_nodes[3].text, "is")
-        
-        self.assertEqual(new_nodes[4].text_type, TextType.text)
-        self.assertEqual(new_nodes[4].text, " a text")
-        
-        self.assertEqual(new_nodes[5].text_type, TextType.code)
-        self.assertEqual(new_nodes[5].text, "this is a `code`")
-
-    def test_bold(self):
-
-        tnode = [TextNode("this **is** a **text**", TextType.text)]
-        new_nodes = split_nodes_delimiter(tnode, '**', TextType.bold)
-        # No delimiter and TextType.text shouldn't fail since tnode is not TextType.text
-        # It only tries to split TextType.text, other tyoes are not transformed.
-
-        self.assertEqual(new_nodes[0].text_type, TextType.text)
-        self.assertEqual(new_nodes[0].text, "this ")
-        
-        self.assertEqual(new_nodes[1].text_type, TextType.bold)
-        self.assertEqual(new_nodes[1].text, "is")
-        
-        self.assertEqual(new_nodes[2].text_type, TextType.text)
-        self.assertEqual(new_nodes[2].text, " a ")
-
-        self.assertEqual(new_nodes[3].text_type, TextType.bold)
-        self.assertEqual(new_nodes[3].text, "text")
-
-        # self.assertEqual(new_nodes[0].text_type, TextType.text)
-        # self.assertEqual(new_nodes[0].text, " a ")
-        # self.assertEqual(new_nodes[1].text_type, TextType.bold)
-        # self.assertEqual(new_nodes[1].text, "text")
-
-    def test_italic(self):
-
-        tnode = [TextNode("this *is* a text", TextType.text)]
-        new_nodes = split_nodes_delimiter(tnode, '*', TextType.italic)
-        # No delimiter and TextType.text shouldn't fail since tnode is not TextType.text
-        # It only tries to split TextType.text, other tyoes are not transformed.
-
-        self.assertEqual(new_nodes[0].text_type, TextType.text)
-        self.assertEqual(new_nodes[0].text, "this ")
-        
-        self.assertEqual(new_nodes[1].text_type, TextType.italic)
-        self.assertEqual(new_nodes[1].text, "is")
-        
-        self.assertEqual(new_nodes[2].text_type, TextType.text)
-        self.assertEqual(new_nodes[2].text, " a text")
-
-    def text_code(self):
-
-        tnode = [TextNode("this `is` a text", TextType.text)]
-        new_nodes = split_nodes_delimiter(tnode, '`', TextType.code)
-        # No delimiter and TextType.text shouldn't fail since tnode is not TextType.text
-        # It only tries to split TextType.text, other tyoes are not transformed.
-
-        self.assertEqual(new_nodes[0].text_type, TextType.text)
-        self.assertEqual(new_nodes[0].text, "this ")
-        
-        self.assertEqual(new_nodes[1].text_type, TextType.code)
-        self.assertEqual(new_nodes[1].text, "is")
-        
-        self.assertEqual(new_nodes[2].text_type, TextType.text)
-        self.assertEqual(new_nodes[2].text, " a text")
-
-    def test_multiple_calls(self):
-
-        tnode = [TextNode("this `is` a **text**", TextType.text)]
-        new_nodes = split_nodes_delimiter(tnode, '`', TextType.code)
-
-        self.assertListEqual(new_nodes, [ TextNode("this ", TextType.text),
-                                         TextNode("is", TextType.code),
-                                         TextNode(" a **text**", TextType.text)
-                                         ])
-
-
-        new_nodes2 = split_nodes_delimiter(new_nodes, '**', TextType.bold)
-
-        self.assertListEqual(new_nodes2, [TextNode("this ", TextType.text),
-                                         TextNode("is", TextType.code),
-                                         TextNode(" a ", TextType.text),
-                                         TextNode("text", TextType.bold)
-                                         ])
 
 if __name__ == "__main__":
     unittest.main()
