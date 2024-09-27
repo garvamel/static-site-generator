1ba80a1b6d47d5ce691c2687ee8b2b53edbfaa38
garvamel <garvamel@gmail.com>
2024-09-27 15:34:14 -0300

    Add block_to_blocktype and aux functions, tests in progress

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,6 @@
 from functools import reduce
+import re
+
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     
@@ -13,3 +15,73 @@ def markdown_to_blocks(markdown: str) -> list[str]:
     
     return blocks
 
+def block_to_blocktype(block):
+
+    '''
+    block types:
+        -paragraph: no symbol
+        -heading: # (up to 6) followed by a space then the text
+        -code: ```code block```
+        -quote: > at the start of every line in the quote block
+        -unordered_list: * or - at the start of every line, followed by a space
+        -ordered_list: incremental number followed by a dot and space every line
+    '''
+    if len(block) == 0:
+        raise ValueError("Empty block")
+    
+    test_funcs = [match_heading, match_code, match_quote, match_unordered_list, match_ordered_list]
+
+    for func in test_funcs:
+        if res := func(block):
+            return res
+    else:
+        return "paragraph"
+
+def match_heading(block):
+
+    if re.findall(r"^#{1,6} ", block):
+        return "heading"
+    else:
+        return False
+    
+def match_code(block):
+
+    if block[0:3] =='```' and block[-3:] == '```':
+        return "code"
+    else:
+        return False
+    
+def match_quote(block):
+
+    for line in block.splitlines():
+        if line[0] =='>':
+            continue
+        else:
+            return False
+        
+    return "quote"
+
+def match_unordered_list(block):
+
+    for line in block.splitlines():
+        if re.findall(r"^[*-] ", line):
+            continue
+        else:
+            return False
+    return "unordered_list"
+
+def match_ordered_list(block):
+    index = 0
+    
+    for line in block.splitlines():
+        match = re.findall(r"^(\d+)\. ", line)
+        if len(match) > 0:
+            if (n := int(match[0])) == index + 1:
+                index = n
+                continue
+            else:
+                return False
+        else:
+            return False
+    else: 
+        return "ordered_list" 
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,7 @@
 import unittest
 
 from block_markdown import (markdown_to_blocks,
+                            block_to_blocktype,
                              )
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -50,3 +51,38 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
                                       """* This is the first list item in a list block
 * This is a list item
 * This is another list item"""])
+        
+class TestBlockToBlockType(unittest.TestCase):
+
+    def test_heading_case(self):
+
+        heading1 = "# This is a heading"
+        heading2 = "## This is a heading"
+        heading3 = "### This is a heading"
+        heading4 = "#### This is a heading"
+        heading5 = "##### This is a heading"
+        heading6 = "###### This is a heading"
+        heading7 = "####### This is a heading"
+
+        self.assertEqual(block_to_blocktype(heading1), "heading")
+        self.assertEqual(block_to_blocktype(heading2), "heading")
+        self.assertEqual(block_to_blocktype(heading3), "heading")
+        self.assertEqual(block_to_blocktype(heading4), "heading")
+        self.assertEqual(block_to_blocktype(heading5), "heading")
+        self.assertEqual(block_to_blocktype(heading6), "heading")
+        self.assertEqual(block_to_blocktype(heading7), "paragraph")
+
+    def test_code_case(self):
+
+        code1 = "```This is a code block```"
+        code2 = "```\nThis\nis\nalso\na\ncode\nblock\n```"
+        code3 = "``This is not a code block``"
+        code4 = "`This is also not a code block`"
+        code5 = "```This is absolutely not a code block"
+
+
+        self.assertEqual(block_to_blocktype(code1), "code")
+        self.assertEqual(block_to_blocktype(code2), "code")
+        self.assertEqual(block_to_blocktype(code3), "paragraph")
+        self.assertEqual(block_to_blocktype(code4), "paragraph")
+        self.assertEqual(block_to_blocktype(code5), "paragraph")
